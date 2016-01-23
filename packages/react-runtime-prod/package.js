74214b5838d223c61c145ed3a3784b62819c8d2f
Tom Kelsey <tomwasd@users.noreply.github.com>
2016-01-23 11:20:46 +0000

    Update package.js

--- packages/react-runtime-prod/package.js
+++ packages/react-runtime-prod/package.js
@@ -12,16 +12,16 @@ Package.describe({
 });
 
 Npm.depends({
-  react                              : "0.14.3",
-  "react-dom"                        : "0.14.3",
+  react                              : "0.14.6",
+  "react-dom"                        : "0.14.6",
   "browserify-replace-g"             : "0.9.1",
-  "react-addons-transition-group"    : "0.14.3",
-  "react-addons-css-transition-group": "0.14.3",
-  "react-addons-linked-state-mixin"  : "0.14.3",
-  "react-addons-create-fragment"     : "0.14.3",
-  "react-addons-update"              : "0.14.3",
-  "react-addons-pure-render-mixin"   : "0.14.3",
-  "react-addons-test-utils"          : "0.14.3"
+  "react-addons-transition-group"    : "0.14.6",
+  "react-addons-css-transition-group": "0.14.6",
+  "react-addons-linked-state-mixin"  : "0.14.6",
+  "react-addons-create-fragment"     : "0.14.6",
+  "react-addons-update"              : "0.14.6",
+  "react-addons-pure-render-mixin"   : "0.14.6",
+  "react-addons-test-utils"          : "0.14.6"
 });
 
 Package.onUse(function (api) {
